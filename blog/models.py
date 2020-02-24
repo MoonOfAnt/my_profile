1c3a286ba639d4b4afbb0091a2b03116baac1d8e
BigAnt <1049205538@qq.com>
2020-02-25 03:24:16 +0800

    创建了一个博客页面

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(default='我的博客的标题', max_length=50)
+    date = models.DateField()
+    imag = models.ImageField(default='default.png', upload_to='image/')
+    text = models.TextField(default='博客正文')
+
+    def __str__(self):
+        return self.title
